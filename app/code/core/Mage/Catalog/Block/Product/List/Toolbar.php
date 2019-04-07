878fffc76d7be2e169bfcbc5febdb171f86410e5
root <root@server-1.localdomain>
2019-04-07 03:10:44 +0000

    sort

--- app/code/core/Mage/Catalog/Block/Product/List/Toolbar.php
+++ app/code/core/Mage/Catalog/Block/Product/List/Toolbar.php
@@ -116,7 +116,7 @@ class Mage_Catalog_Block_Product_List_Toolbar extends Mage_Core_Block_Template
      *
      * @var string
      */
-    protected $_direction           = 'asc';
+    protected $_direction           = 'desc';
 
     /**
      * Default View mode
